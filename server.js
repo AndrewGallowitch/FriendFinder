566ac368f223c1ed19ff828c9a1e4dec7c4c11a9
Drew Ross <42723693+AndrewGallowitch@users.noreply.github.com>
2018-10-18 22:15:55 -0400

    update changes to deploy to heroku

--- server.js
+++ server.js
@@ -2,8 +2,9 @@
 var express = require('express');
 var path = require('path');
 
+
 var app = express();
-var PORT = 8080;
+var PORT = process.env.PORT || 3000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
